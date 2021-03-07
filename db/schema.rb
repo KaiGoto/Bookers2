3091c694a9c8c9040ce5ae9e9fa9b12d724904ee
EC2 Default User <ec2-user@ip-172-31-69-92.ec2.internal>
2021-03-07 11:13:13 +0000

    kadai5

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_05_010713) do
+ActiveRecord::Schema.define(version: 2021_03_07_073527) do
 
   create_table "book_comments", force: :cascade do |t|
     t.text "comment"
@@ -45,6 +45,14 @@ ActiveRecord::Schema.define(version: 2021_03_05_010713) do
     t.index ["follow_id"], name: "index_relationships_on_follow_id"
     t.index ["user_id", "follow_id"], name: "index_relationships_on_user_id_and_follow_id", unique: true
     t.index ["user_id"], name: "index_relationships_on_user_id"
+  end
+
+  create_table "searches", force: :cascade do |t|
+    t.string "name"
+    t.string "title"
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
