aa09cb5230a21f826a142e32672685f6ef10ee21
EC2 Default User <ec2-user@ip-172-31-69-92.ec2.internal>
2021-03-09 05:31:24 +0000

    課題6

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,14 +1,19 @@
 class FavoritesController < ApplicationController
+  before_action :set_book
   def create
-    @book_show = Book.find(params[:book_id])
-    favorite = current_user.favorites.new(book_id: @book_show.id)
+    @book = Book.find(params[:book_id])
+    favorite = current_user.favorites.new(book_id: @book.id)
     favorite.save
-    redirect_to request.referer
+    # redirect_to request.referer
   end
   def destroy
-    @book_show = Book.find(params[:book_id])
-    favorite = current_user.favorites.find_by(book_id: @book_show.id)
+    @book = Book.find(params[:book_id])
+    favorite = current_user.favorites.find_by(book_id: @book.id)
     favorite.destroy
-    redirect_to request.referer
+    # redirect_to request.referer
+  end
+  private
+  def set_book
+    @book = Book.find(params[:book_id])
   end
 end
